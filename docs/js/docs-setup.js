bbd12b695da8c18caae60d0afc5e6c5c4778bdf5
Varya Stepanova <mail@varya.me>
2014-09-16 11:29:58 +0300

    Documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "supersonic.debug:ping",
+      "shortName": "ping",
+      "type": "service",
+      "moduleName": "supersonic",
+      "shortDescription": "Checks promise resolving",
+      "keywords": "api checks debug promise resolving service supersonic"
+    },
+    {
+      "section": "api",
+      "id": "supersonic.debug:ping",
+      "shortName": "ping",
+      "type": "service",
+      "moduleName": "supersonic",
+      "shortDescription": "Checks promise resolving",
+      "keywords": "api checks debug promise resolving service supersonic"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
