2d03daa58c2e44349db130571cb70046da631e77
Petrus Repo <petrus.repo@iki.fi>
2015-05-08 16:10:25 +0300

    feat (super-data-repeat) Support users of Enterprise apps

---
+++ testApp/app/common/assets/appgyver.js
@@ -0,0 +1,26 @@
+window.appgyver = {
+  environment: {
+    auth: {
+      endpoint: "https://rest-auth.devgyver.com/api/v2"
+    },
+    data: {
+      bundle: {
+        options: {
+          baseUrl: "https://proxy-api.devgyver.com/v3/",
+          headers: {
+            steroidsAppId: "1026",
+            steroidsApiKey: "8ce9e528ab7ff3813bcca6fc1a372c03f99eb677871ee1cdebaa60d005eddcfc",
+          }
+        },
+        resources: {}
+      }
+    }
+  }
+};
+
+window.localStorage.setItem("__ag:data:session", JSON.stringify({
+  access_token: "264b9bf21e62dfbf09533b44293c0d58",
+  user_details: {
+    id: 981
+  }
+}));
