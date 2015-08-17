086dc9374e7d9800d180ab1462b6efc76ca1abf1
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2015-08-17 17:39:19 +0300

    chore (testApp) Update mocked appgyver.js by copying from testSpecApp

--- testApp/app/common/assets/appgyver.js
+++ testApp/app/common/assets/appgyver.js
@@ -5,14 +5,93 @@ window.appgyver = {
     },
     data: {
       bundle: {
-        options: {
-          baseUrl: "https://proxy-api.devgyver.com/v3/",
-          headers: {
-            steroidsAppId: "1026",
-            steroidsApiKey: "8ce9e528ab7ff3813bcca6fc1a372c03f99eb677871ee1cdebaa60d005eddcfc",
+        "options": {
+          "baseUrl": "https://rest-api.appgyver.com/v3/",
+          "headers": {
+            "steroidsApiKey": "71fcb87a3c5bea2982997080ebe5d329b1ff37e2f7884d8c4b2471301fc6bb40",
+            "steroidsAppId": 52559
           }
         },
-        resources: {}
+        "resources": {
+          "SandboxTask": {
+            "schema": {
+              "fields": {
+                "description": {
+                  "type": "string"
+                },
+                "completed": {
+                  "type": "boolean"
+                },
+                "id": {
+                  "type": "string",
+                  "identity": true
+                }
+              },
+              "identifier": "id"
+            }
+          },
+          "BuiltIOTask": {
+            "schema": {
+              "fields": {
+                "updated_at": {
+                  "type": "string"
+                },
+                "created_at": {
+                  "type": "string"
+                },
+                "description": {
+                  "type": "string"
+                },
+                "completed": {
+                  "type": "boolean"
+                },
+                "ACL": {
+                  "type": "object"
+                },
+                "tags": {
+                  "type": "array"
+                },
+                "updated_by": {
+                  "type": "string"
+                },
+                "created_by": {
+                  "type": "string"
+                },
+                "app_user_object_uid": {
+                  "type": "string"
+                },
+                "published": {
+                  "type": "boolean"
+                },
+                "uid": {
+                  "type": "string",
+                  "identity": true
+                },
+                "_version": {
+                  "type": "integer"
+                }
+              },
+              "identifier": "uid"
+            }
+          },
+          "SandboxFileResource": {
+            "schema": {
+              "fields": {
+                "description": {
+                  "type": "string"
+                },
+                "file": {
+                  "type": "file"
+                },
+                "id": {
+                  "type": "string",
+                  "identity": true
+                }
+              },
+              "identifier": "id"
+            }
+          }
+        }
       }
     }
   }
